8f65e6c2ef6d22593ee68fad4ec02d498ce8e197
Higor-P <69658736+Higor-P@users.noreply.github.com>
2020-08-14 16:48:31 -0300

    Add files via upload

---
+++ aula02.php
@@ -0,0 +1,18 @@
+<?php
+function main () {
+        $num = 0;
+        $r   = 0;
+        $e   = 0;
+        
+        $num = readline("informe um número \n");
+        while ($num < 0){
+             $num = readline("informe um número \n");
+        }
+
+        $e = pow($num, $num);
+        $r = sqrt($num);
+
+       echo "potencia: $e \n";
+       echo "raiz do $num $r";
+
+    }
